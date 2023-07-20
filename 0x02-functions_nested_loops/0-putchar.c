d2b1d2c6530b70e9cba1232f198bf27d01a34738
root <touufy>
2023-07-20 10:34:37 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+*main - function print _putchar  followed by a new line.
+*
+*Return: returns 0
+*/
+int main(void)
+{
+	char arr[9] = "_putchar";
+
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(arr[i]);
+	}
+	printf("\n");
+	return (0);
+}
